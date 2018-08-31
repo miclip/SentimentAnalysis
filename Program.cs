f53c0c10bb1beb95e7a436ea1a5c419df51437a7
Michael Lipscombe <mlipscombe@outlook.com>
2018-08-31 09:59:49 -0600

    working model

--- Program.cs
+++ Program.cs
@@ -1,12 +1,91 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Models;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
 
 namespace SentimentAnalysis
 {
     class Program
     {
-        static void Main(string[] args)
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "wikipedia-detox-250-line-data.tsv");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "wikipedia-detox-250-line-test.tsv");
+        static readonly string _modelpath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var model = await Train();
+            Evaluate(model);
+            Predict(model);
         }
+
+        public static async Task<PredictionModel<SentimentData, SentimentPrediction>> Train()
+        {
+            var pipeline = new LearningPipeline();
+            pipeline.Add(new TextLoader(_dataPath).CreateFrom<SentimentData>());
+            pipeline.Add(new TextFeaturizer("Features", "SentimentText"));
+            pipeline.Add(new FastTreeBinaryClassifier() );
+            PredictionModel<SentimentData, SentimentPrediction> model =
+                pipeline.Train<SentimentData, SentimentPrediction>();
+            await model.WriteAsync(_modelpath);
+            return model;
+        }
+
+        public static void Evaluate(PredictionModel<SentimentData, SentimentPrediction> model)
+        {
+            var testData = new TextLoader(_testDataPath).CreateFrom<SentimentData>();
+            var evaluator = new BinaryClassificationEvaluator();
+            BinaryClassificationMetrics metrics = evaluator.Evaluate(model, testData);
+            Console.WriteLine();
+            Console.WriteLine("PredictionModel quality metrics evaluation");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.Auc:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+        }
+
+        public static void Predict(PredictionModel<SentimentData, SentimentPrediction> model)
+        {
+            IEnumerable<SentimentData> sentiments = new []
+            {
+                new SentimentData
+                {
+                SentimentText = "Please refrain from adding nonsense to Wikipedia."
+                },
+                new SentimentData
+                {
+                SentimentText = "He is the best, and the article should say that."
+                },
+                new SentimentData
+                {
+                SentimentText = "Mike, I don't want you doing that!"
+                },
+                new SentimentData
+                {
+                SentimentText = "Umm... well ... it's kinda too late. BJ asked for time off requests ages ago."
+                },
+
+            };
+
+            IEnumerable<SentimentPrediction> predictions = model.Predict(sentiments);
+
+            Console.WriteLine();
+            Console.WriteLine("Sentiment Predictions");
+            Console.WriteLine("---------------------");
+
+            var sentimentsAndPredictions = sentiments.Zip(predictions, (sentiment, prediction) =>(sentiment, prediction));
+
+            foreach (var item in sentimentsAndPredictions)
+            {
+                Console.WriteLine($"Sentiment: {item.sentiment.SentimentText} | Prediction: {(item.prediction.Sentiment ? "Positive" : "Negative")}");
+            }
+            Console.WriteLine();
+        }
+
     }
 }
